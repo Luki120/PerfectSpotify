a17c1608f7d4a56630818b6fa2bfe3ecd560b07c
Luki120 <74214115+Luki120@users.noreply.github.com>
2020-12-01 22:39:02 -0300

    Add files via upload

---
+++ spotiprefs/MiscellaneousRootListController.h
@@ -0,0 +1,20 @@
+#import <Preferences/PSListController.h>
+#import <Preferences/PSSpecifier.h>
+#import <CepheiPrefs/HBRootListController.h>
+#import <CepheiPrefs/HBAppearanceSettings.h>
+#import <Cephei/HBPreferences.h>
+#import <Preferences/PSTableCell.h>
+
+
+
+@interface MiscellaneousRootListController : PSListController
+@end
+
+@interface NowPlayingUIRootListController : PSListController
+@end
+
+@interface CarModeUIRootListController : PSListController
+@end
+
+@interface SearchPageRootListController : PSListController
+@end
---
+++ spotiprefs/MiscellaneousRootListController.m
@@ -0,0 +1,52 @@
+#include "MiscellaneousRootListController.h"
+
+@implementation MiscellaneousRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [self loadSpecifiersFromPlistName:@"Miscellaneous" target:self];
+	}
+
+	return _specifiers;
+}
+
+@end
+
+
+@implementation NowPlayingUIRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [self loadSpecifiersFromPlistName:@"Now Playing UI" target:self];
+	}
+
+	return _specifiers;
+}
+
+@end
+
+
+@implementation CarModeUIRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [self loadSpecifiersFromPlistName:@"Car Mode UI" target:self];
+	}
+
+	return _specifiers;
+}
+
+@end
+
+
+@implementation SearchPageRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [self loadSpecifiersFromPlistName:@"Search Page" target:self];
+	}
+
+	return _specifiers;
+}
+
+@end
